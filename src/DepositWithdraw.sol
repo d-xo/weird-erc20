88653877beb977e3c57f7656e88ca95e4c2f6e59
SIDHARTH20K4 <91970588+SIDHARTH20K4@users.noreply.github.com>
2024-02-29 19:36:10 +0530

    contract updated!!
    used .transfer instead of .call for security purpose.

--- src/DepositWithdraw.sol
+++ src/DepositWithdraw.sol
@@ -4,28 +4,30 @@ pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract DepositWithdraw is Ownable(msg.sender), ReentrancyGuard {
+contract TransferOwnership is Ownable(msg.sender), ReentrancyGuard {
     mapping(address => uint256) private _balances;
 
-    event Deposited(address indexed depositor, uint256 amount);
-    event Withdrew(address indexed withdrawer, uint256 amount);
+    event Deposit(address indexed depositor, uint256 amount);
+    event Withdraw(address indexed withdrawer, uint256 amount);
+    event ownershipTransferred(address indexed owner, address indexed newOwner);
 
     function deposit() public payable {
         require(msg.value > 0, "Deposit amount must be greater than 0");
         _balances[msg.sender] += msg.value;
-        emit Deposited(msg.sender, msg.value);
+        emit Deposit(msg.sender, msg.value);
     }
 
     function withdraw(uint256 amount) public nonReentrant {
         require(amount <= _balances[msg.sender], "Insufficient balance");
         _balances[msg.sender] -= amount;
-        (bool success, ) = msg.sender.call{value: amount}("");
-        require(success, "Transfer failed.");
-        emit Withdrew(msg.sender, amount);
+        payable(msg.sender).transfer(amount);
+        emit Withdraw(msg.sender, amount);
     }
 
     function transferOwnership(address newOwner) public onlyOwner override {
-        transferOwnership(newOwner);
+        require(newOwner == address(0),"invalid address");
+        super.transferOwnership(newOwner);
+        emit ownershipTransferred(msg.sender, newOwner);
     }
 
     function getBalance() public view returns (uint256) {
